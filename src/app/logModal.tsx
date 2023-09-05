edc2e20e73f45ef466058c6376c1b4446479a1d9
이여진 <jinspace3710@gmail.com>
2023-09-06 00:10:09 +0900

    feat: 파일 날짜 추출 기능 구현

---
+++ src/app/logModal.tsx
@@ -0,0 +1,111 @@
+import dayjs from 'dayjs';
+import EmojiPicker, { EmojiClickData } from 'emoji-picker-react';
+import { useState } from 'react';
+
+export default function LogModal(setOpenCreateCinemaLog: any) {
+  const [showEmojiPickerButton, setShowEmojiPickerButton] = useState(false);
+  const [emojiPicked, setEmojiPicked] = useState('❔');
+  const searchMovie = () => {};
+  const [logDate, setLogDate] = useState<String>('');
+  const showEmojiPicker = (event: any) => {
+    event.preventDefault();
+    setShowEmojiPickerButton(!showEmojiPickerButton);
+  };
+
+  const inputLogFile = (event: React.ChangeEvent<HTMLInputElement> | null) => {
+    if (event != null && event.target.files) {
+      let date = dayjs(event.target.files[0].lastModified);
+
+      setLogDate(date.format('YYYY-MM-DD'));
+    }
+  };
+  const changeEmojiPicked = (emojiData: EmojiClickData, event: MouseEvent) => {
+    setEmojiPicked(emojiData.emoji);
+    setShowEmojiPickerButton(!showEmojiPickerButton);
+  };
+
+  return (
+    <>
+      <div className='fixed top-0 left-0 w-full h-full  bg-green-200 flex   justify-center items-center'>
+        <div className='w-full max-w-xs'>
+          <form className='sw-full max-w-lg  '>
+            <label htmlFor='formFile' className='mb-2 inline-block text-black '>
+              그 날의 로그 사진을 업로드 해주세요
+            </label>
+            <input
+              className='relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100dark:focus:border-primary'
+              type='file'
+              id='formFile'
+              onChange={inputLogFile}
+            />
+            <div className='flex flex-wrap -mx-3 mt-3'>
+              <div className='w-full px-3'>
+                <label
+                  className='block uppercase tracking-wide text-gray-700 font-bold mb-2'
+                  htmlFor='log_date'
+                >
+                  날짜
+                </label>
+                <p
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                  id='log_date'
+                >
+                  {logDate}
+                </p>
+              </div>
+            </div>
+            <div className='flex flex-wrap -mx-3 mt-3'>
+              <div className='w-full px-3'>
+                <label
+                  className='block uppercase tracking-wide text-gray-700 font-bold mb-2'
+                  htmlFor='search_movie'
+                >
+                  영화 검색
+                </label>
+                <input
+                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                  placeholder='영화 이름을 검색해주세요.'
+                  id='search_movie'
+                  onChange={searchMovie}
+                ></input>
+              </div>
+            </div>
+            <div className='flex flex-wrap -mx-3 mt-3'>
+              <div className='w-full h-full px-3'>
+                <label
+                  className='block uppercase tracking-wide text-gray-700 mb-2'
+                  htmlFor='search_movie'
+                >
+                  그 날의 기분을 골라주세요.
+                </label>
+                <div className='w-full h-full flex justify-center items-center'>
+                  <button
+                    className='w-fit h-fit rounded-full'
+                    onClick={showEmojiPicker}
+                  >
+                    {emojiPicked}
+                  </button>
+                </div>
+                {showEmojiPickerButton && (
+                  <EmojiPicker onEmojiClick={changeEmojiPicked} />
+                )}
+              </div>
+            </div>
+          </form>
+          <div className='flex flex-wrap -mx-3 mt-6'>
+            <div className='w-full h-full px-3'>
+              <div className='w-full h-full flex justify-center items-center'>
+                <button
+                  className=' w-full h-full bg-black'
+                  onClick={showEmojiPicker}
+                >
+                  create log
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import dayjs, { Dayjs } from 'dayjs';
 
 import { useEffect, useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
-import { format } from 'path';
 import Image from 'next/image';
+import LogModal from './logModal';
 
 let colStartClasses = [
   '',
@@ -35,7 +35,8 @@ export default function Home() {
     today.format('MMMM YYYY')
   );
   const [daysInCurrentMonth, setDaysInCurrentMonth] = useState<Dayjs[]>([]);
-  const [openCreateCinemaLog, setOpenCreateCinemaLog] = useState<boolean>(true);
+  const [openCreateCinemaLog, setOpenCreateCinemaLog] =
+    useState<boolean>(false);
   let dateOfMonth: Dayjs = dayjs(currentMonth).date(1);
 
   let cinemaLogsOfselectedDay = cinemaLogs.filter(cinemaLog =>
@@ -82,253 +83,131 @@ export default function Home() {
 
   const createCinemaLog = () => {
     //정보 입력 팝업 결정
-    setOpenCreateCinemaLog(false);
+    setOpenCreateCinemaLog(true);
   };
+
   return (
     <>
-      {openCreateCinemaLog ? (
-        <div className='pt-16'>
-          <div className='max-w-md px-4 mx-auto sm:px-7 md:max-w-4x1 md:px-6'>
-            <div className='md:grid md:grid-cols-2 md:divide-x md:divide-gray-200'>
-              <div className='md:pr-14'>
-                <div className='flex items-center'>
-                  <h2 className='flex-auto font-semibold text-gray-900'>
-                    {dateOfMonth.format('MMMM YYYY')}
-                  </h2>
-                  <button
-                    type='button'
-                    onClick={previousMonth}
-                    className='-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'
+      <div className='pt-16'>
+        <div className='max-w-md px-4 mx-auto sm:px-7 md:max-w-4x1 md:px-6'>
+          <div className='md:grid md:grid-cols-2 md:divide-x md:divide-gray-200'>
+            <div className='md:pr-14'>
+              <div className='flex items-center'>
+                <h2 className='flex-auto font-semibold text-gray-900'>
+                  {dateOfMonth.format('MMMM YYYY')}
+                </h2>
+                <button
+                  type='button'
+                  onClick={previousMonth}
+                  className='-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'
+                >
+                  <span className='sr-only'>Previous month</span>
+                  <ChevronLeftIcon className='w-5 h-5' aria-hidden='true' />
+                </button>
+                <button
+                  onClick={nextMonth}
+                  type='button'
+                  className='-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'
+                >
+                  <span className='sr-only'>Next month</span>
+                  <ChevronRightIcon className='w-5 h-5' aria-hidden='true' />
+                </button>
+              </div>
+              <div className='grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500'>
+                <div>S</div>
+                <div>M</div>
+                <div>T</div>
+                <div>W</div>
+                <div>T</div>
+                <div>F</div>
+                <div>S</div>
+              </div>
+              <div className='grid grid-cols-7 mt-2 text-sm'>
+                {daysInCurrentMonth.map((day, dayIndex) => (
+                  <div
+                    key={day.toString()}
+                    // className={`${colStartClasses[day.get('day')]} py-1.5`}
+                    className={filterClassNames(
+                      dayIndex === 0 && colStartClasses[day.get('day')],
+                      'py-1.5'
+                    )}
                   >
-                    <span className='sr-only'>Previous month</span>
-                    <ChevronLeftIcon className='w-5 h-5' aria-hidden='true' />
-                  </button>
-                  <button
-                    onClick={nextMonth}
-                    type='button'
-                    className='-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500'
-                  >
-                    <span className='sr-only'>Next month</span>
-                    <ChevronRightIcon className='w-5 h-5' aria-hidden='true' />
-                  </button>
-                </div>
-                <div className='grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500'>
-                  <div>S</div>
-                  <div>M</div>
-                  <div>T</div>
-                  <div>W</div>
-                  <div>T</div>
-                  <div>F</div>
-                  <div>S</div>
-                </div>
-                <div className='grid grid-cols-7 mt-2 text-sm'>
-                  {daysInCurrentMonth.map((day, dayIndex) => (
-                    <div
-                      key={day.toString()}
-                      // className={`${colStartClasses[day.get('day')]} py-1.5`}
+                    <button
+                      type='button'
+                      onClick={selectedDayButtonClick(day)}
                       className={filterClassNames(
-                        dayIndex === 0 && colStartClasses[day.get('day')],
-                        'py-1.5'
+                        day.isSame(selectedDay) && 'text-white',
+                        !day.isSame(selectedDay) &&
+                          day.isSame(dayjs(), 'day') &&
+                          'text-red-500',
+                        !day.isSame(selectedDay) &&
+                          !day.isSame(dayjs(), 'day') &&
+                          day.isSame(dateOfMonth, 'month') &&
+                          'text-gray-900',
+                        !day.isSame(selectedDay) &&
+                          !day.isSame(dayjs(), 'day') &&
+                          !day.isSame(dateOfMonth, 'month') &&
+                          'text-gray-400',
+                        day.isSame(selectedDay) &&
+                          day.isSame(dayjs(), 'day') &&
+                          'bg-red-500',
+                        day.isSame(selectedDay) &&
+                          !day.isSame(dayjs(), 'day') &&
+                          'bg-gray-900',
+                        !day.isSame(selectedDay) && 'hover:bg-gray-200',
+                        (day.isSame(selectedDay) ||
+                          day.isSame(dayjs(), 'day')) &&
+                          'font-semibold',
+                        'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
                       )}
                     >
-                      <button
-                        type='button'
-                        onClick={selectedDayButtonClick(day)}
-                        className={filterClassNames(
-                          day.isSame(selectedDay) && 'text-white',
-                          !day.isSame(selectedDay) &&
-                            day.isSame(dayjs(), 'day') &&
-                            'text-red-500',
-                          !day.isSame(selectedDay) &&
-                            !day.isSame(dayjs(), 'day') &&
-                            day.isSame(dateOfMonth, 'month') &&
-                            'text-gray-900',
-                          !day.isSame(selectedDay) &&
-                            !day.isSame(dayjs(), 'day') &&
-                            !day.isSame(dateOfMonth, 'month') &&
-                            'text-gray-400',
-                          day.isSame(selectedDay) &&
-                            day.isSame(dayjs(), 'day') &&
-                            'bg-red-500',
-                          day.isSame(selectedDay) &&
-                            !day.isSame(dayjs(), 'day') &&
-                            'bg-gray-900',
-                          !day.isSame(selectedDay) && 'hover:bg-gray-200',
-                          (day.isSame(selectedDay) ||
-                            day.isSame(dayjs(), 'day')) &&
-                            'font-semibold',
-                          'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
-                        )}
-                      >
-                        <time dateTime={day.format('YYYY-MM-DD')}>
-                          {day.get('date')}
-                        </time>
-                      </button>
-                    </div>
-                  ))}
-                </div>
+                      <time dateTime={day.format('YYYY-MM-DD')}>
+                        {day.get('date')}
+                      </time>
+                    </button>
+                  </div>
+                ))}
               </div>
-              <section className='mt-12 md:mt-0 md:p1-14'>
-                <h2 className='font-semibold text-gray-900'>
-                  <time dateTime={selectedDay.format('YYYY-MM-DD')}>
-                    {selectedDay.format('YYYY년 MM월 DD일')}
-                  </time>{' '}
-                  <p></p>
-                  cinema log...
-                </h2>
-                <ol className='mt-4 space-y-1 text-sm leading-6 text-gray-500'>
-                  {cinemaLogsOfselectedDay.length > 0 ? (
-                    cinemaLogsOfselectedDay.map(cinemalog => (
-                      <Image
-                        key={cinemalog.id}
-                        src={cinemalog.imageUrl}
-                        alt={cinemalog.name}
-                        width={300}
-                        height={300}
-                      ></Image>
-                    ))
-                  ) : (
-                    <p>
-                      Would you like to create a log?
-                      <button
-                        onClick={createCinemaLog}
-                        type='button'
-                        className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-3 py-1.5 ml-2 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'
-                      >
-                        Yes
-                      </button>
-                    </p>
-                  )}
-                </ol>
-              </section>
             </div>
+            <section className='mt-12 md:mt-0 md:pl-14'>
+              <h2 className='font-semibold text-gray-900'>
+                <time dateTime={selectedDay.format('YYYY-MM-DD')}>
+                  {selectedDay.format('YYYY년 MM월 DD일')}
+                </time>{' '}
+                <p></p>
+                cinema log...
+              </h2>
+              <ol className='mt-4 space-y-1 text-sm leading-6 text-gray-500'>
+                {cinemaLogsOfselectedDay.length > 0 ? (
+                  cinemaLogsOfselectedDay.map(cinemalog => (
+                    <Image
+                      key={cinemalog.id}
+                      src={cinemalog.imageUrl}
+                      alt={cinemalog.name}
+                      width={300}
+                      height={300}
+                    ></Image>
+                  ))
+                ) : (
+                  <p>There&apos;s no log...</p>
+                )}
+              </ol>
+            </section>
+          </div>
+          <div className='flex justify-center items-center text-black'>
+            Would you like to create a log?
+            <button
+              onClick={createCinemaLog}
+              type='button'
+              className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-3 py-1.5 ml-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'
+            >
+              Yes
+            </button>
           </div>
         </div>
-      ) : (
-        <div className='w-full max-w-xs'>
-          <form className='w-full max-w-lg'>
-            <label
-              htmlFor='formFile'
-              className='mb-2 inline-block text-black dark:text-neutral-200'
-            >
-              그 날의 로그 사진을 업로드 해주세요
-            </label>
-            <input
-              className='relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary'
-              type='file'
-              id='formFile'
-            />
-            <div className='flex flex-wrap -mx-3 mb-6'>
-              <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  htmlFor='grid-first-name'
-                >
-                  First Name
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-                  id='grid-first-name'
-                  type='text'
-                  placeholder='Jane'
-                ></input>
-                <p className='text-red-500 text-xs italic'>
-                  Please fill out this field.
-                </p>
-              </div>
-              <div className='w-full md:w-1/2 px-3'>
-                <label
-                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  htmlFor='grid-last-name'
-                >
-                  Last Name
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-last-name'
-                  type='text'
-                  placeholder='Doe'
-                ></input>
-              </div>
-            </div>
-            <div className='flex flex-wrap -mx-3 mb-6'>
-              <div className='w-full px-3'>
-                <label
-                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  htmlFor='grid-password'
-                >
-                  Password
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-password'
-                  type='password'
-                  placeholder='******************'
-                ></input>
-                <p className='text-gray-600 text-xs italic'>
-                  Make it as long and as crazy as you'd like
-                </p>
-              </div>
-            </div>
-            <div className='flex flex-wrap -mx-3 mb-2'>
-              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  htmlFor='grid-city'
-                >
-                  City
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-city'
-                  type='text'
-                  placeholder='Albuquerque'
-                ></input>
-              </div>
-              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  htmlFor='grid-state'
-                >
-                  State
-                </label>
-                <div className='relative'>
-                  <select
-                    className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                    id='grid-state'
-                  >
-                    <option>New Mexico</option>
-                    <option>Missouri</option>
-                    <option>Texas</option>
-                  </select>
-                  <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
-                    <svg
-                      className='fill-current h-4 w-4'
-                      xmlns='http://www.w3.org/2000/svg'
-                      viewBox='0 0 20 20'
-                    >
-                      <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-              <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-                <label
-                  className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                  htmlFor='grid-zip'
-                >
-                  Zip
-                </label>
-                <input
-                  className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-zip'
-                  type='text'
-                  placeholder='90210'
-                ></input>
-              </div>
-            </div>
-          </form>
-        </div>
+      </div>
+      {openCreateCinemaLog && (
+        <LogModal setOpenCreateCinemaLog={setOpenCreateCinemaLog}></LogModal>
       )}
     </>
   );
